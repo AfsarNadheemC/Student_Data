6b5adc32bea745ace83b95fcacea9529049dd0de
Afsar Nadheem C <afsar17052002@gmail.com>
2025-09-21 23:10:42 +0530

    ExamClass Added

---
+++ Student_Data/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Student_Data
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class Exam
+        {
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+            public float Language1 { get; set; }
+            public float Language2 { get; set; }
+            public float Maths { get; set; }
+            public float Science { get; set; }
+            public float SocialStudies { get; set; }
+            public float SubjectMaxMark { get; set; }
+            public float TotalScored { get; set; }
+            public float TotalMarks { get; set; }
+        }
+
+    }
+}
